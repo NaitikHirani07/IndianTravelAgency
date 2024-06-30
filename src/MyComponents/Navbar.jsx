202c677be0d77c4dbcf05ec6b2b602f294a4f1d4
NaitikHirani07 <naitikhirani58@gmail.com>
2024-06-30 17:19:18 +0530

    four commit

--- src/MyComponents/Navbar.jsx
+++ src/MyComponents/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <Link className="nav-link fw-bold text-dark" to="/header">Home</Link>
+                                <Link className="nav-link fw-bold text-dark" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="nav-link fw-bold text-dark" to="/category">Category</Link>
